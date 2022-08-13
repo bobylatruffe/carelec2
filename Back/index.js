6ad22570c59d7c19ef5ac1e8d28d75eb6b948a6e
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-13 15:27:52 +0200

    Ajout de 3 api côté serveur, afin de rechercher une motorisation si elle n'est pas trouvé par la plaque d'immat

--- Back/index.js
+++ Back/index.js
@@ -2,14 +2,33 @@ const express = require('express');
 const app = express();
 
 const getRevision = require('./estimerUnEntretien/getRevision');
+const { getModels, getMarques, getMotorisations } = require('./estimerUnEntretien/manualGetMotorisation');
 
 const cl = console.log;
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/api/getInfosVehicule/:numPlaque", (req, resp) => {
   getRevision(req.params.numPlaque)
     .then(revisions => resp.send(revisions))
     .catch(marques => resp.send(marques))
 });
+
+app.get("/api/getMotorisations/:model", (req, resp) => {
+  resp.send(getMotorisations(req.params.model))
+})
+
+app.get("/api/getModels/:marque", (req, resp) => {
+  resp.send(getModels(req.params.marque));
+})
+
+app.get("/api/getMarques", (req, resp) => {
+  resp.send(getMarques());
+})
 
 app.listen(5000, () => {
   cl("Je suis à l'écoute sur le port 5000");
