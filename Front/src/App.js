418bfc0eb08f2a2b8a928aa683a062e7064ab9a0
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-14 15:35:05 +0200

    Revert "entrainement pour utiliser gooogle firebase auth et firebase cloud firestore"
    This reverts commit 60ba51152a0768890ed5c4e91e104616c1b1f502.

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,12 +1,10 @@
-import Connexion from "./Pages/Connexion/Connexion";
 import EstimerUnEntretien from "./Pages/EstimerUnEntretien/EstimerUnEntretien";
 
 function App() {
   return (
     <div>
       <h1>Carelec</h1>
-      {/* <EstimerUnEntretien /> */}
-      <Connexion />
+      <EstimerUnEntretien />
     </div>
   );
 }
