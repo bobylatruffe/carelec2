6e5ff5b33293b927f5938356739501e71a5264c2
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-15 09:09:55 +0200

    Utilisation de Firebase Auth et Firestore pour créer un compte :   - il faut encore décider quoi ecrire dans la base ...

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,10 +1,12 @@
 import EstimerUnEntretien from "./Pages/EstimerUnEntretien/EstimerUnEntretien";
+import Connexion from '../src/Pages/Connexion/Connexion';
 
 function App() {
   return (
     <div>
       <h1>Carelec</h1>
       <EstimerUnEntretien />
+      <Connexion />
     </div>
   );
 }
---
+++ Front/src/Pages/Connexion/firebaseUtil.js
@@ -0,0 +1,63 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getDoc, getFirestore, doc, setDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsXcKRkIJJVZD5AUBDkmbf165Qrt_brGU",
+  authDomain: "carelec-3c305.firebaseapp.com",
+  projectId: "carelec-3c305",
+  storageBucket: "carelec-3c305.appspot.com",
+  messagingSenderId: "588882631000",
+  appId: "1:588882631000:web:73c4299f249f86dad3ad2d"
+};
+
+// permet de se connecter à mon applicaiton firebase (cf - firebaseConfig)
+const app = initializeApp(firebaseConfig);
+
+
+// Création, Ajout, Connexion depuis l'API Auth de Firebase
+// --------------------------------------------------------------------------------------
+// https://firebase.google.com/docs/reference/js/auth.md#getauth
+// permet d'avoir une instance de Auth de mon application firebase
+const auth = getAuth();
+
+//https://firebase.google.com/docs/reference/js/auth.md#createuserwithemailandpassword
+// wraper afin de créer un utilisateur dans Auth (Promise !)
+const signUp = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password)
+}
+// wraper afin de connecter un utilisatuer depuis Auth (Promise !)
+const signIn = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+//https://firebase.google.com/docs/reference/js/auth.md#signout
+const signOut = () => {
+  signOut(auth);
+}
+
+
+// Gestion de la bdd depuis Firebase firestore
+// --------------------------------------------------------------------------------------
+const db = getFirestore();
+
+// créer un user dans firestore
+const createUser = (userAuth, infosUser) => {
+  if (!userAuth)
+    return
+
+  console.log("lol");
+  const refDoc = doc(db, "users", userAuth.uid);
+  console.log("ptdr");
+
+  getDoc(refDoc)
+    .then(userSnapShot => {
+      if (!userSnapShot.exists()) {
+        setDoc(refDoc, {
+          ...infosUser,
+        })
+      }
+    }).catch(err => console.log(err.code));
+}
+
+export { signUp, signIn, createUser }
