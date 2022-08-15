258251de52ff35de2c689d08866bca581d22768b
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-15 14:21:52 +0200

    Recodage de se module, avant c'était trop n'importe quoi

--- Back/estimerUnEntretien/searchModelInFileFs.js
+++ Back/estimerUnEntretien/searchModelInFileFs.js
@@ -2,7 +2,7 @@
  * Nom ...................... : searchModelInFileFs.js
  * Rôle ..................... : 
  * 
-Le but de ce "module" est de trouver le fichier contenant tous les types de modèle (motirisation, chevaux) d'un véhicule donnée.
+Le but de ce "module" est de trouver le fichier contenant tous les types de modèle (motirisation, chevaux, cylindre...) d'un véhicule donnée.
 Son unique paramètre est une chaîne représentant un objet "standardisé" pour notre projet via makeMarqueModelCv.js. Ci-après un exemple de résultat :
 {
   "80 Avant 2.0 E 115cv": {
@@ -36,118 +36,138 @@ Son unique paramètre est une chaîne représentant un objet "standardisé" pour
  * Autheur .................. : Bozlak Fatih 1503001522G
  * Licence .................. : IED L2 Informatique
  * Année .................... : 2021/2022
- * Usage .................... : node searchModelInFileFs.js ['string standardisé par makeMarqueModelCv.js' | TEST]
+ * Usage .................... : node searchModelInFileFs.js 'string standardisé par makeMarqueModelCv.js'
  **/
 
-// if (process.argv[2] == "TEST") {
-//   if (process.argv[3] == undefined) {
-//     console.log("Usage: \n\tnode searchModelInFileFs.js TEST nom_fichier.data\n");
-//     return
-//   }
+const fs = require("fs");
+const cl = console.log;
 
-//   let readline = require('readline');
-//   let fs = require('fs');
+let currentDir = null;
 
-//   let myInterface = readline.createInterface({
-//     input: fs.createReadStream(process.argv[3])
-//   });
+function getMarquesInFs() {
+  let marquesInFs = null;
 
-//   myInterface.on('line', function (line) {
-//     // console.log(line);
-//     searchModelInFileFs(line);
-//   });
-
-//   return
-// }
-
-// if (process.argv[2] == undefined) {
-//   console.log("Usage: \n\tnode searchModelInFileFs.js 'string_strandardisé_par_makeMarqueModelCv.js' | TEST nom_fichier_standardisé.data\n")
-//   return;
-// }
-
-
-
-
-// searchModelInFileFs(process.argv[2]);
-
-function searchModelInFileFs(inputDataStandardise) {
-  const fs = require('fs');
-  cl = console.log;
-
-  let marqueModelCv = JSON.parse(inputDataStandardise);
-
-  let baseDir = "./Carnets d'entretiens";
-
-  // trouver le dossier qui correspond à la marque 
-  let dirs = fs.readdirSync(baseDir).map(elem => elem.toLowerCase());
-  for (marqueDir of dirs) {
-    if (marqueDir == marqueModelCv.marque) {
-      baseDir += "/" + marqueDir;
-    }
+  try {
+    marquesInFs = fs.readdirSync(currentDir).map(elem => elem.toLowerCase())
+  } catch (err) {
+    console.log("ERROR:\n\t Aucune marques trouvée dans " + currentDir);
+    return null;
   }
 
-  // trouver le modèle de la marque
-  let motorisationsPossible = [];
-  let modelsFiles = fs.readdirSync(baseDir).map(elem => elem.toLowerCase());
-  for (model of modelsFiles) {
-    if (marqueModelCv.model.includes(model.split(".")[0])) {
-      motorisationsPossible.push(baseDir + "/" + model)
-    }
-  }
-
-  // parcours de chaque model possible (un model contient plusieurs motorisation)
-  let listeMotor;
-  let max = [];
-  for (motorisationFile of motorisationsPossible) {
-    let motorisationRaw = fs.readFileSync(motorisationFile);
-    let motorisations = JSON.parse(motorisationRaw);
-    listeMotor = Object.keys(motorisations);
-    max = [];
-    let currentMax = 0;
-    for (let i = 0; i < listeMotor.length; i++) {
-      if (listeMotor[i].includes(marqueModelCv.cv + "")) {
-        max.push({
-          valeur: trouveMaxCorrespndance(listeMotor[i]),
-          motor: listeMotor[i],
-        })
-      }
-    }
-    if (max.length == 1) {
-      // console.log(max[0].motor);
-      // console.log(motorisations[max[0].motor])
-      return ({
-        motorisation: max[0].motor,
-        revisions: motorisations[max[0].motor]
-      });;
-    }
-
-    if (max.length > 1) {
-      max.sort((a, b) => {
-        return b.valeur - a.valeur;
-      })
-
-      // cl(max);
-      // console.log({
-      //   motorisation: max[0].motor, 
-      //   revisions: motorisations[max[0].motor]
-      // });
-      return ({
-        motorisation: max[0].motor,
-        revisions: motorisations[max[0].motor]
-      });
-    }
-  }
-
-  function trouveMaxCorrespndance(motor) {
-    let i = 0;
-    let tokens = motor.split(" ").map(elem => elem.toLowerCase());
-    for (token of tokens) {
-      if (marqueModelCv.model.includes(token))
-        i++;
-    }
-
-    return i;
-  }
+  return marquesInFs;
 }
 
-module.exports = searchModelInFileFs 
+// Permet de vérifier que le models est présent dans la bdd
+function getModelsInFs(marquesInFs, marqueUser) {
+  let modelsInFs = null;
+
+  for (let i = 0; i < marquesInFs.length; i++) {
+    if (marquesInFs[i] === marqueUser) {
+      currentDir += marquesInFs[i] + "/";
+      break;
+    }
+    if (i === marquesInFs.length - 1) {
+      console.log("ERROR:\n\t getmodelsInFs(): la marque rechercher par l'utilisateur n'est pas dans la bdd");
+      return null;
+    }
+  }
+
+  try {
+    modelsInFs = fs.readdirSync(currentDir);
+  } catch (err) {
+    console.log("ERROR:\n\t getmodelsInFs(): Aucun models trouvée dans " + currentDir);
+    return null;
+  }
+
+  return modelsInFs;
+}
+
+// retourne le un objet : 
+//    le nombre de correspondance de chaîne dans chaîne 2
+//    chaîne2
+function bestMatch(chaine1, chaine2) {
+  let point = 0;
+  chaine1 = chaine1.split(" ");
+  chaine2 = chaine2.split(" ");
+
+  for (sousChaine1 of chaine1) {
+    for (sousChaine2 of chaine2) {
+      if (sousChaine1 === sousChaine2.toLowerCase())
+        point++;
+    }
+  }
+
+  return { point: point, libelle: chaine2.join(" ") };
+}
+
+function getMotorisationRevisionsInfs(objStandardise) {
+  let motorisationRevisionsInfs = null;
+
+  try {
+    motorisationRevisionsInfs = fs.readFileSync(currentDir);
+  } catch (err) {
+    console.log("ERROR:\n\t Impossible de lire le fichier " + currentDir);
+    return null;
+  }
+
+  let motorisationsRevisionsInJson = JSON.parse(motorisationRevisionsInfs);
+
+  // permet de sélectionner les motorisations ayant le mm nb chevaux que celui de l'user
+  let motorisationsLibelles = Object.keys(motorisationsRevisionsInJson).filter(motorLibelle => {
+    return (objStandardise.cv === (parseInt(motorLibelle.split(" ").slice(-1)[0].slice(0, -2))));
+  });
+
+  let tabPointLibelle = [];
+  for (motor of motorisationsLibelles) {
+    tabPointLibelle.push(bestMatch(objStandardise.model, motor));
+  }
+  tabPointLibelle.sort((a, b) => b.point - a.point)
+
+  return ({
+    motorisation: tabPointLibelle[0].libelle,
+    revisionsKm: motorisationsRevisionsInJson[tabPointLibelle[0].libelle].km,
+  });
+}
+
+// Permet de trouver le model (fichier) correspondant au maximum au modelUser
+function getModelInFs(modelsInFs, objStandardise) {
+  let motorisationsInFs = null;
+  let modelTrouveInFs = null;
+  let modelsPossible = []
+
+  for (let i = 0; i < modelsInFs.length; i++) {
+    if (objStandardise.model.includes(modelsInFs[i].split(".json")[0])) {
+      modelTrouveInFs = modelsInFs[i];
+      break;
+    }
+  }
+
+  currentDir += modelTrouveInFs // side effect
+
+  return getMotorisationRevisionsInfs(objStandardise);
+}
+
+function getMotorisationRevisionsKm(nomStandardise) {
+  let objStandardise = JSON.parse(nomStandardise);
+
+  currentDir = "./Carnets d'entretiens/";
+
+  let marquesInFs = getMarquesInFs();
+  if (!marquesInFs)
+    return;
+
+  let modelsInFs = getModelsInFs(marquesInFs, objStandardise.marque);
+  if (!modelsInFs)
+    return;
+
+  let motorisationRevisionsKm = getModelInFs(modelsInFs, objStandardise);
+
+  if (process.argv[2])
+    cl(motorisationRevisionsKm);
+  return motorisationRevisionsKm;
+}
+
+if (process.argv[2])
+  getMotorisationRevisionsKm(process.argv[2])
+
+module.exports = getMotorisationRevisionsKm 
