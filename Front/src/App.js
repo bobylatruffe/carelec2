692852b6168e1ce4e291fe03e7c54b7750f51f25
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-16 12:17:59 +0200

    Tentative de création de la page 'mon compte'

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,11 +1,13 @@
 import EstimerUnEntretien from "./Composants/EstimerUnEntretien/EstimerUnEntretien";
+import MonCompte from "./Composants/MonCompte/MonCompte";
 
 function App() {
   return (
     <div>
       <h1>Carelec</h1>
-      <EstimerUnEntretien />
+      {/* <EstimerUnEntretien /> */}
       {/* <Connexion /> */}
+      <MonCompte />
     </div>
   );
 }
---
+++ Front/src/Composants/MonCompte/MonCompte.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MonCompte extends React.Component {
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Mon compte</h1>
+      </div>
+    )
+  }
+}
+
+export default MonCompte;
