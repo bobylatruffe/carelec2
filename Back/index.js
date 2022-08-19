85481c9d6b956f65b5cc5ec9d185f4a97e29a9b7
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-19 19:06:09 +0200

    C'est de la merde ! Tout est de la putaine merde, je suis juste un con, je suis perdu dans mon propre code, tout ça psk j'ai pas de fil directrice et que je connais pas bien react et nodejs.
    Enfin bref, je vais recommencer pour essayer d'avoir du code plus que propre et en même temps rédiger mon rapport, il reste 10 jours !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Back/index.js
+++ Back/index.js
@@ -1,26 +1,105 @@
 const express = require('express');
-const polyUtil = require('polyline-encoded');
 const app = express();
-
-const getRevision = require('./estimerUnEntretien/getRevision');
-const { getModels, getMarques, getMotorisations } = require('./estimerUnEntretien/manualGetMotorisation');
+const polyUtil = require('polyline-encoded');
+const fs = require('fs');
 
 const cl = console.log;
+
+
+
+
+
+app.use(function (req, res, next) {
+  // pour une utilisation local
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
+
+
+
+
+
+
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const pathBase = 'img/etatDesLieux/';
+    try {
+      fs.mkdirSync(pathBase + req.body.immat);
+    } catch (err) {
+      // on ignore les erreurs pour l'instant;
+      // notamment pour EEXIST
+    }
+    cb(null, 'img/etatDesLieux/' + req.body.immat)
+  },
+  filename: function (req, file, cb) {
+    let nameFile =
+      req.body.posXY +
+      "-" +
+      Math.round(Math.random() * 1E5) +
+      Date.now() +
+      ".png"
+    cb(null, nameFile)
+  }
+})
+const upload = multer({ storage, })
+
+app.post('/api/setEtatDesLieuxImmat/', upload.any('etatsDesLieux'), function (req, res, next) {
+  res.send({ status: 200 })
+})
+
+
+
+function getEtatDesLieuxImg(immat) {
+  let listeImg = null;
+  try {
+    listeImg = fs.readdirSync(__dirname + "/img/etatDesLieux/" + immat);
+  } catch (err) {
+  }
+
+  if(listeImg) 
+  return listeImg
+
+  return {status: 404}
+}
+
+app.use("/", express.static("img"));
+app.get('/api/getEtatDesLieuxImmat/:immat', (req, resp) => {
+  let immat = req.params.immat;
+  let listesImg = getEtatDesLieuxImg(immat);
+
+
+
+  resp.send(listesImg);
+});
+
+
+
 
 let simulationGps = [];
 function getGaragistePos() {
   if (simulationGps.length === 0)
     simulationGps = polyUtil.decode("ayhgHcbkn@R}GsB_CGwEvFEjCt\\cCbz@{Pd~@jc@b_@{Rpv@mBdBeA}AbBaCvHlHjd@pl@zHtOvF|XfEdy@pS|h@rBvOdCnAj[yHxt@gZ~WbKpj@tEvw@`i@pExJpQ~x@hSrOh@yF");
-    
+
 
   return simulationGps.shift();
 }
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.get("/api/getGaragistePos/", (req, resp) => {
+  resp.send(getGaragistePos());
 });
+
+
+
+
+
+const getRevision = require('./estimerUnEntretien/getRevision');
+const { getModels, getMarques, getMotorisations } = require('./estimerUnEntretien/manualGetMotorisation');
 
 app.get("/api/getInfosVehicule/:numPlaque", (req, resp) => {
   getRevision(req.params.numPlaque)
@@ -40,13 +119,16 @@ app.get("/api/getMarques", (req, resp) => {
   resp.send(getMarques());
 });
 
-app.get("/api/getGaragistePos/", (req, resp) => {
-  resp.send(getGaragistePos());
-});
+
+
 
 app.get("/*", (req, resp) => {
   resp.send({ status: 404 })
 });
+
+
+
+
 
 app.listen(5000, () => {
   cl("Je suis à l'écoute sur le port 5000");
